f97e79b40faf47a00a4c9b51f1af0871a55de18a
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-07-27 12:27:10 +0600

    updates

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
 
-document.getElementById('pin').addEventListener("click", function(){
-    var fourdigitnumber= Math.floor(1000 + Math.random() * 9000);
+document.getElementById('pin-generate').addEventListener("click", function(){
+    var fourDigitNumber= Math.floor(1000 + Math.random() * 9000);
 
-    document.getElementById("pinOut").value = fourdigitnumber;
+    document.getElementById("pin-display").value = fourDigitNumber;
 })

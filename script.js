58a256d002c002c8c3bc88df564e67bc20614c19
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-07-28 23:15:24 +0600

    script updates

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-
-
 //function for generate random number
 document.getElementById('pin-generate').addEventListener("click", function(){
     var fourDigitNumber= Math.floor(1000 + Math.random() * 9000);
@@ -8,20 +5,38 @@ document.getElementById('pin-generate').addEventListener("click", function(){
     document.getElementById("pin-display").value = fourDigitNumber;
 })
 
-
-// select all the number button and add event Listener
-const numberBtn = document.querySelectorAll(".number_btn");
-numberBtn.forEach((item) => {
-  item.addEventListener("click", () => {
-    appendNumber(item);
+ //function for add number beside a number in user input.
+const buttons = document.querySelectorAll(".number_btn");
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function(event){
+    let values = event.target.innerText;
+    let userInputShow = document.getElementById("user_input_show");
+    userInputShow.value += values;
   });
-});
-
-//function for add number beside a number in user input.
-function appendNumber(number) {
-  let userInputShow = document.querySelector(".user_input_show");
-  userInputShow.value = userInputShow.value + number.innerText;
 }
+
+// function handle(event){
+//     let values = event.target.innerText;
+//     let userInputShow = document.getElementById("user_input_show");
+//     userInputShow.value += values;
+    
+    
+// }
+
+
+// // select all the number button and add event Listener
+// const getNumber = document.querySelectorAll(".number_btn");
+// getNumber.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     appendNumber(item);
+//   });
+// });
+
+// //function for add number beside a number in user input.
+// function appendNumber(number) {
+//   let userInputShow = document.querySelector(".user_input_show");
+//   userInputShow.value = userInputShow.value + number.innerText;
+// }
 
 
 // All clear from user input
